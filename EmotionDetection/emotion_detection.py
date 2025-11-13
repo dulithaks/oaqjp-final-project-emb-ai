5ac970b16b4cf17fffe022ec5ef3cdb19bf75b5f
Dulitha K <dulithaks@gmail.com>
2025-11-13 10:47:21 -0500

    lint fix

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,16 @@ def emotion_detector(text_to_analyze):
         headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     )
 
+    if response.status_code == 400:
+        return {
+            "anger": None, 
+            "disgust": None, 
+            "fear": None, 
+            "joy": None, 
+            "sadness": None, 
+            "dominant_emotion": None
+        }
+
     res = response.json()
     emotions = res['emotionPredictions'][0]["emotion"]
     emotions["dominant_emotion"] = max(emotions, key=emotions.get)
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+Server
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +8,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    index
+    """
     return render_template("index.html")
 
 @app.route('/emotionDetector')
 def get_emotion_by_text():
+    """
+    get emotion by text
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    return emotion_detector(text_to_analyze)
+    res = emotion_detector(text_to_analyze)
 
+    if res["dominant_emotion"] is None:
+        return {"message": "Invalid text! Please try again!"}
+
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
